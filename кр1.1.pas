1d86660e2115b582fccf2c9d0e1f2bc8f860e11c
ripkakaka <117993259+ripkakaka@users.noreply.github.com>
2022-11-11 17:05:05 +0300

    Add files via upload

---
+++ кр1.1.pas
@@ -0,0 +1,10 @@
+﻿var x, y: real;
+begin
+  writeln('Введите x:');
+  readln(x);
+  if x < -9 then y:= power(x, (0.1*x))*cos(x)-power(x, 2)
+  else if (-9<=x) and (x<-2) then y:= power(x, (1/3))/sin(x)-49/sin(x)
+  else if (-2<=x) and (x<2) then y:= sin(x)/tan(x)-ln(x)/power(x,(1/3))
+  else if 2<=x then y:= 20/sin(x)-14/power(e, x);
+  writeln(y)
+end.
---
+++ кр1.2.pas
@@ -0,0 +1,24 @@
+﻿var x, y, c: real;
+begin
+  x:= -11;
+  y:= 4;
+  while x<=y do 
+    begin
+    if x < -9 then begin 
+    c:= power(x, (0.1*x))*cos(x)-power(x, 2)
+    end
+    else if (-9<=x) and (x<-2) then begin 
+    c:= power(x, (1/3))/sin(x)-49/sin(x)
+    end
+    else if (-2<=x) and (x<2) then begin
+    c:= sin(x)/tan(x)-ln(x)/power(x,(1/3))
+    end
+    else if 2<=x then begin
+    c:= 20/sin(x)-14/power(e, x);
+    end;
+    writeln('При x = ' ,x:0:1, '   y = ' , c);
+    x:=x+0.3
+end;
+readln;
+end.
+    
